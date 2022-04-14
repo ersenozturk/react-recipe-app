f853c5033971372c621e3d510117801e2ce30cf6
ersen <70ozturkersen@gmail.com>
2022-04-14 11:31:28 +0300

    App.js completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+        <AppRouter/>
+    </div>
+  );
+}
+
+export default App;
